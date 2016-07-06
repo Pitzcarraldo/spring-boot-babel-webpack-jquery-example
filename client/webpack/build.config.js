27f233791f7414e290a71a0cfc1e4def954269b8
Minkyu Cho <mrnoname@naver.com>
2016-07-06 10:42:25 +0900

    Fix loader setting

--- client/webpack/build.config.js
+++ client/webpack/build.config.js
@@ -20,7 +20,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: ['babel', 'eslint']
+        loaders: ['babel', 'eslint']
       },
       {
         test: /\.css$/,
--- client/webpack/devServer.config.js
+++ client/webpack/devServer.config.js
@@ -22,7 +22,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: ['babel', 'eslint']
+        loaders: ['babel', 'eslint']
       },
       {
         test: /\.css$/,
